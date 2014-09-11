bfe6dd13e861c7f49cd680f3da5a4da795796fcc
Ryan Smith <0ryansmith1994@gmail.com>
2014-09-11 13:12:07 +0100

    Uses environment variables to support forge.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -5,12 +5,15 @@ use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder {
 
-	public function run()
-	{
+	public function run() {
+		$name = getenv('user_name') ? getenv('user_name') : 'Ryan';
+		$email = getenv('user_email') ? getenv('user_email') : 'ryan.smith@ht2.co.uk';
+		$password = getenv('user_password') ? getenv('user_password') : 'password';
+
 		User::create([
-			'name' => 'Ryan',
-			'email' => 'ryan.smith@ht2.co.uk',
-			'password' => Hash::make('password')
+			'name' => $name,
+			'email' => $email,
+			'password' => Hash::make($password)
 		]);
 	}
 
