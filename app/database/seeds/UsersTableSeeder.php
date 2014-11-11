b12e5ac90d2fb91dae8c88a4ff95fd50c6248b10
Ryan Smith <0ryansmith1994@gmail.com>
2014-11-11 13:25:54 +0000

    Improves seeder.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -6,8 +6,8 @@ use Faker\Factory as Faker;
 class UsersTableSeeder extends Seeder {
 
 	public function run() {
-		$name = getenv('user_name') ? getenv('user_name') : 'Ryan';
-		$email = getenv('user_email') ? getenv('user_email') : 'ryan.smith@ht2.co.uk';
+		$name = getenv('user_name') ? getenv('user_name') : 'Dummy';
+		$email = getenv('user_email') ? getenv('user_email') : 'ex@mple.co.uk';
 		$password = getenv('user_password') ? getenv('user_password') : 'password';
 
 		User::create([
